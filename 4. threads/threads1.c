d753757d9422ffdbb43b36737aa7abe345a5f8aa
Eng. Nagy Khairat <nagy@aast.edu>
2024-10-26 09:40:14 +0300

    auto

---
+++ 4. threads/threads1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+void* routine(void* raw_args){
+	int i;
+	for(i=0;i<5;i++){
+		printf("B: %d\n", i);
+		sleep(1);
+	}
+}
+int main(){
+	pthread_t tid;
+	pthread_create(&tid, NULL, routine, NULL);
+	int i;
+	for(i=0;i<5;i++){
+		printf("A: %d\n", i);
+		usleep(0.2 * 1000000);
+	}
+	pthread_exit(NULL);
+	return 0;
+}
